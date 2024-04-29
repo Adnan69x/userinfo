36d4d3a0bf4928befe848830c13477a16d4b0dba
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 21:38:53 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = os.getenv("27913018")
-API_HASH = os.getenv("04e2f4e414cdabe52ad985adaa6cfe09")
-BOT_TOKEN = os.getenv("6865095206:AAHSZsiD_O8IzqguUBBVPe6XZWqr5Z843Wk")
+API_ID = getenv("27913018")
+API_HASH = getenv("04e2f4e414cdabe52ad985adaa6cfe09")
+BOT_TOKEN = getenv("6865095206:AAHSZsiD_O8IzqguUBBVPe6XZWqr5Z843Wk")
