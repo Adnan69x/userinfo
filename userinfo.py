ba2ca44a7afa84d8990ab2604915a8166a08118d
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 09:55:01 +0600

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -9,33 +9,26 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['info'])
 async def user_info(message: types.Message):
-    # Extract the mentioned username from the command
-    target_username = message.get_args()
+    # Get the mentioned username after the command
+    mentioned_username = message.get_args()
     
-    if not target_username:
+    if not mentioned_username:
         await message.reply("Please specify a username after the command.")
         return
     
     if message.chat.type == "private":
         # If the command is sent in a private chat, retrieve the information of the user who sent the command
         user = message.from_user
-        user_id = user.id
-        full_name = user.full_name
-        username = user.username
     else:
-        # If the command is sent in a group, check if the mentioned user is the same as the user who sent the command
-        mentioned_user = await bot.get_chat(target_username)
-        if mentioned_user.id == message.from_user.id:
-            user = message.from_user
-        else:
-            user = mentioned_user
-        
-        user_id = user.id
-        full_name = user.full_name
-        username = user.username
+        # If the command is sent in a group, retrieve the information of the mentioned user
+        user = await bot.get_chat(mentioned_username)
+    
+    user_id = user.id
+    full_name = user.full_name
+    username = user.username
     
     await message.reply(
-        f"ID: {user_id}\n"
+        f"User ID: {user_id}\n"
         f"Full Name: {full_name}\n"
         f"Username: {username}"
     )
