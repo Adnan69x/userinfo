de46bc70218e68367f3269ec28976be2c1c8ad83
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 21:37:58 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = os.getenv("TELEGRAM_API_ID")
-API_HASH = os.getenv("TELEGRAM_API_HASH")
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+API_ID = os.getenv("27913018")
+API_HASH = os.getenv("04e2f4e414cdabe52ad985adaa6cfe09")
+BOT_TOKEN = os.getenv("6865095206:AAHSZsiD_O8IzqguUBBVPe6XZWqr5Z843Wk")
