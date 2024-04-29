2dd915a8a9e13c2bf3076f5879399517da16cd7a
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 18:12:00 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = 'API_ID' 
+API_ID = "API_ID" 
 # Replace 'your_api_id' with your actual Telegram API ID.
-API_HASH = 'API_HASH'  
+API_HASH = "API_HASH" 
 # Replace 'your_api_hash' with your actual Telegram API HASH.
-BOT_TOKEN = 'BOT_TOKEN'  
+BOT_TOKEN = "BOT_TOKEN"
 # Replace 'your_bot_token' with your actual Telegram BOT token.
